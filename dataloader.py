4c7115b0154d2c5f010eb11bfc86f9b73e75b324
Kabir Ray Malik <kabirraymalik@gmail.com>
2024-02-13 21:30:33 -0500

    saved this time

--- dataloader.py
+++ dataloader.py
@@ -1,3 +1,16 @@
 import torch
 import main 
+import os
+import cv2
+
+def generateFrames(numFrames):
+    path = os.getcwd()
+    if os.path.exists(path + "data/image_data/") == False:
+        os.mkdir(path+"data/image_data/")
+    videoData = os.fsencode(path+"data/video_data/")
+    for label in os.listdir(videoData):
+        os.mkdir(path+"data/image_data/"+label+"/")
+        for video in os.listdir(videoData+label+"/"):
+            os.mkdir(path+"data/image_data/" + label + "/" + video)
+            #TODO: extract frames from video with cv2
 
